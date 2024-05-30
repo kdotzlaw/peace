e1feaadd247931f08a24250ceeb4bbe87e6b3284
Katrina Dotzlaw <dotzlawk@myumanitoba.ca>
2024-05-30 16:08:48 -0500

    bones of homepage

--- home.py
+++ home.py
@@ -11,17 +11,32 @@ class Home(QMainWindow):
         self.label = QLabel("Home")
         #set window size to be screen width
         self.showMaximized()
-        #create widget collection
-        layout = QVBoxLayout()
-        text = QLabel("Test",alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(text)
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        layout = QHBoxLayout()
+        #create dropdown for table selection
+        self.cb = QComboBox()
+        self.cb.addItem("volunteers")
+        self.cb.addItem("jobs")
+        #self.cb.currentIndexChanged.connect(self.showTableOptions)
+        #add dropdown to layout
+        layout.addWidget(self.cb)
 
+        # based on dropdown selection, display options for queries
+        # on query click, open dialog box with form to fillout (if needed)
+        # on form submit (if needed), display results below
+
+        #set widget layout
+        self.setLayout(layout)
+        self.show()
+
+
+
+
+
+    def showTableOptions(self, text):
+        print("showing options for ",text)
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication(sys.argv)
     gui=Home()
     gui.show()
-    app.exec()
+    sys.exit(app.exec())
